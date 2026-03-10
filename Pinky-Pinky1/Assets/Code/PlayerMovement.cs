46d27afc49a04db0329ace6fb4a6d17704cf2917
gloomii-kat <kat.mashego24@gmail.com>
2026-03-10 12:44:05 +0200

    Tiles & Nav mesh
    Added a tile pallete and tiles so i have made the scene and stuff.
    
    Also added nav mesh so that i can use it for the child

---
+++ Pinky-Pinky1/Assets/Code/PlayerMovement.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+    ///public AwarenessScript awarenessScript;
+   /// public Slider slider;
+
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        moveInput = new Vector2(
+            Input.GetAxisRaw("Horizontal"),
+            Input.GetAxisRaw("Vertical")
+        ).normalized;
+
+       // awarenessScript.slider.value = awarenessScript.awarenessLevel;
+
+
+    }
+
+    void FixedUpdate()
+    {
+        rb.linearVelocity = moveInput * moveSpeed;
+    }
+}
